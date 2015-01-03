43ec104fcfe73ddbdec17095a6e70a11d858a52a
Qix <i.am.qix@gmail.com>
2015-01-03 10:55:47 -0700

    added leveldb to repo

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 function die {
-  echo "$*" >&2
+  echo -e "\n$*" >&2
   exit 1
 }
 
 # Make lua
 make --directory=lib/lua generic \
   || die "Building lua failed!"
+
+# Make levelDB
+make --directory=lib/leveldb libleveldb.a \
+  || die "Building LevelDB failed!"
