1b7e5f5295bf76a90a8c0e1ba179cbc2b44fc811
Qix <i.am.qix@gmail.com>
2015-04-12 07:44:05 -0400

    updated some documentation

--- src/db.h
+++ src/db.h
@@ -5,17 +5,41 @@
 
 typedef struct back_db back_db;
 
+/**
+ * Opens (or creates) the Back database
+ */
 back_db* back_db_open(void);
+
+/**
+ * Closes the Back database
+ */
 void back_db_close(back_db*);
 
+/**
+ * Sets a key to a string value
+ */
 int back_db_set(back_db*, char*, char*);
+
+/**
+ * Sets a key to an int value
+ */
 int back_db_set_int(back_db*, char*, long int);
-// below function malloc's output string; remember to free
+
+/**
+ * Gets a string value by a key
+ *  NOTE: malloc's the resulting string. Make sure to free!
+ */
 int back_db_get(back_db*, char*, char**);
+
+/**
+ * Gets a long value by a key
+ */
 int back_db_get_int(back_db*, char*, long int*, long int);
 
-/* PLATFORM DEPENDENT CALLS */
-// returned string, if not null, must be freed by caller
+/**
+ * Gets the path to the Back database folder
+ *  NOTE: malloc's the resuting string (unless it's 0). Make sure to free.
+ */
 char* back_db_find(char*);
 
 #endif
