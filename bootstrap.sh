efa75dd87e130bcdbb2bb7dcde6a3f13282f8b77
Qix <i.am.qix@gmail.com>
2015-04-12 07:32:05 -0400

    added snappy and fixed up bootstrapper

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,6 +13,18 @@ fi
 make --directory=lib/lua $_clean generic \
   || die "Building lua failed!"
 
+# Make snappy
+if [ ! -f lib/snappy/Makefile ]; then
+  (cd lib/snappy && ./autogen.sh && ./configure) \
+    || die "Configuring Snappy failed!"
+fi
+make --directory=lib/snappy $_clean all \
+  || die "Building Snappy failed!"
+
+# Link snappy
+[ -d lib/snappy/bin ] || mkdir lib/snappy/bin
+cp -r lib/snappy/.libs/* lib/snappy/bin/
+
 # Make levelDB
 make --directory=lib/leveldb $_clean libleveldb.a \
   || die "Building LevelDB failed!"
